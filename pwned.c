e544f6c8a4ffdabcc3c364c00375aa5d0c8f36f7
Piotr Czajkowski <pczajkowski@gmail.com>
2020-04-01 13:08:18 +0200

    RTFM

--- pwned.c
+++ pwned.c
@@ -15,8 +15,7 @@ char *getURL(const char* hash) {
 	char *url = malloc(URL_SIZE);
 	if (url == NULL) return NULL;
 
-	size_t toCopy = BASEURL_SIZE;
-	strncpy(url, BASEURL, toCopy);
+	strncpy(url, BASEURL, URL_SIZE - 1);
 	strncat(url, hash, HASH_PREFIX_LENGTH);
 
 	return url;
